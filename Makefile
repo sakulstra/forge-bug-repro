81880b0ec3d2ae0e9599c4725829e212b656e6f7
Lukas <lukasstrassel@googlemail.com>
2023-03-26 13:30:47 +0200

    feat: add reproduction

--- Makefile
+++ Makefile
@@ -14,3 +14,5 @@ download :; cast etherscan-source --chain ${chain} -d src/etherscan/${chain}_${a
 git-diff :
 	@mkdir -p diffs
 	@printf '%s\n%s\n%s\n' "\`\`\`diff" "$$(git diff --no-index --diff-algorithm=patience --ignore-space-at-eol ${before} ${after})" "\`\`\`" > diffs/${out}.md
+
+reproduction:; forge script scripts/Ghost.s.sol:Deploy --rpc-url mainnet -vv --sender 0x25F2226B597E8F9514B3F68F00f494cF4f286491
--- scripts/Ghost.s.sol
+++ scripts/Ghost.s.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.0;
 
 import {Script} from 'forge-std/Script.sol';
-import {Ghost} from '../src/contracts/Ghost.sol';
+import {Pool} from 'aave-v3-core/contracts/protocol/pool/Pool.sol';
+import {IPoolAddressesProvider} from 'aave-v3-core/contracts/interfaces/IPoolAddressesProvider.sol';
+
 
 contract Deploy is Script {
   function run() external {
     vm.startBroadcast();
-    new Ghost();
+    new Pool(IPoolAddressesProvider(address(42)));
     vm.stopBroadcast();
   }
 }
